40aca9d3b1932cd374359d20a6f04fa9ff86855b
Gibran Iqbal <gibran.iqbal91@gmail.com>
2024-08-28 09:43:46 -0500

    wireup stdout and stderr for child process

--- app/main.go
+++ app/main.go
@@ -8,15 +8,13 @@ import (
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
-
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 	cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
-	if err != nil {
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
 		fmt.Printf("Err: %v", err)
 		os.Exit(1)
 	}
-
-	fmt.Println(string(output))
 }
