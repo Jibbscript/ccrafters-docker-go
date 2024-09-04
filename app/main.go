1f8a63bd82d2516ab48af4c0f79ddf2a49b59dd8
Gibran Iqbal <gibran.iqbal91@gmail.com>
2024-09-04 00:02:55 -0700

    lint: clean up extra line

--- app/main.go
+++ app/main.go
@@ -83,7 +83,6 @@ func getLayerBlob(image, blobSum, token string) error {
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
-
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 	cmd := exec.Command(command, args...)
